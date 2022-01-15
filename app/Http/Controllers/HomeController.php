a00e2d1a0a042a495739be57bf340cf15ecfcda0
Kien <ntkien.hust@gmail.com>
2022-01-15 21:48:58 +0700

    home controller added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,10 +9,53 @@ use App\Product;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $categories = Category::where('status', '=', 0)->get();
         $brands = Brand::where('status', '=', 0)->get();
         $products = Product::where('status', '=', 0)->orderByDesc('id')->paginate(6);
         return view('pages.home.index',  ['products' => $products, 'categories' => $categories, 'brands' => $brands]);
+    }
+
+    public function showCategory($slug)
+    {
+        $category = Category::where('slug', $slug)->first();
+        $name = $category->name;
+
+        $categories = Category::where('status', '=', 0)->get();
+        $brands = Brand::where('status', '=', 0)->get();
+        $products = Product::where('status', '=', 0)->where('category_id', $category->id)->paginate(9);
+        return view('pages.home.category', ['name' => $name, 'products' => $products, 'categories' => $categories, 'brands' => $brands]);
+    }
+
+    public function showBrand($slug)
+    {
+        $brand = Brand::where('slug', $slug)->first();
+        $name = $brand->name;
+
+        $categories = Category::where('status', '=', 0)->get();
+        $brands = Brand::where('status', '=', 0)->get();
+        $products = Product::where('status', '=', 0)->where('brand_id', '=', $brand->id)->paginate(9);
+        return view('pages.home.brand', ['name' => $name, 'products' => $products, 'categories' => $categories, 'brands' => $brands]);
+    }
+
+    public function postSearch(Request $request)
+    {
+        $keywords = $request->keywords;
+
+        $categories = Category::where('status', '=', 0)->get();
+        $brands = Brand::where('status', '=', 0)->get();
+        $search_products = Product::where('name', 'like', '%' . $keywords . '%')->where('status', '=', 0)->paginate(9);
+
+        return view('pages.home.search', ['keywords' => $keywords, 'products' => $search_products, 'categories' => $categories, 'brands' => $brands]);
+    }
+
+    public function showDetail($slug)
+    {
+        $categories = Category::where('status', '=', 0)->get();
+        $brands = Brand::where('status', '=', 0)->get();
+        $product = Product::where('slug', '=', $slug)->first();
+        return view('pages.home.detail', ['product' => $product, 'categories' => $categories, 'brands' => $brands]);
+
     }
 }
---
+++ resources/views/pages/home/detail.blade.php
@@ -0,0 +1,205 @@
+@extends('pages.layouts.index')
+@section('content')
+    <div class="product-details">
+        <!--product-details-->
+        <div class="col-sm-5">
+            <div class="view-product">
+                <img src="{{ URL::to('/public/uploads/product/' . $product->image) }}" alt="" />
+                <h3>ZOOM</h3>
+            </div>
+            <div id="similar-product" class="carousel slide" data-ride="carousel">
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+
+                    <div class="item active">
+                        <a href=""><img src="{{ ('public/frontend/images/similar1.jpg') }}" alt=""></a>
+                        <a href=""><img src="{{ ('public/frontend/images/similar2.jpg') }}" alt=""></a>
+                        <a href=""><img src="{{ ('public/frontend/images/similar3.jpg') }}" alt=""></a>
+                    </div>
+                </div>
+
+                <!-- Controls -->
+                <a class="left item-control" href="#similar-product" data-slide="prev">
+                    <i class="fa fa-angle-left"></i>
+                </a>
+                <a class="right item-control" href="#similar-product" data-slide="next">
+                    <i class="fa fa-angle-right"></i>
+                </a>
+            </div>
+
+        </div>
+        <div class="col-sm-7">
+            <div class="product-information">
+                <!--/product-information-->
+                <img src="images/product-details/new.jpg" class="newarrival" alt="" />
+                <h2>{{ $product->name }}</h2>
+                <p>ID: {{ $product->id }}</p>
+                <img src="images/product-details/rating.png" alt="" />
+
+                <form action="{{ URL::to('/save-cart') }}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="hidden" value="{{ $product->id }}" class="cart_product_id_{{ $product->id }}">
+                    <input type="hidden" value="{{ $product->name }}" class="cart_product_name_{{ $product->id }}">
+                    <input type="hidden" value="{{ $product->image }}" class="cart_product_image_{{ $product->id }}">
+                    <input type="hidden" value="{{ $product->quantity }}"
+                        class="cart_product_quantity_{{ $product->id }}">
+                    <input type="hidden" value="{{ $product->price }}" class="cart_product_price_{{ $product->id }}">
+
+                    <span>
+                        <span>{{ number_format($product->price, 0, ',', '.') . 'VNĐ' }}</span>
+
+                        <label>Quantity:</label>
+                        <input name="qty" type="number" min="1" class="cart_product_qty_{{ $product->id }}" value="1" />
+                        <input name="productid_hidden" type="hidden" value="{{ $product->id }}" />
+                    </span>
+                    <input type="button" value="Add to Cart" class="btn btn-primary btn-sm add-to-cart"
+                        data-id_product="{{ $product->id }}" name="add-to-cart">
+                </form>
+
+                <p><b>Available:</b>Yes</p>
+                <p><b>Condition:</b>100% new</p>
+                <p><b>In Stock:</b> {{ $product->quantity }}</p>
+                <p><b>Brand:</b> {{ $product->brand->name }}</p>
+                <p><b>Category:</b> {{ $product->category->name }}</p>
+                <a href=""><img src="images/product-details/share.png" class="share img-responsive" alt="" /></a>
+            </div>
+            <!--/product-information-->
+        </div>
+    </div>
+    <!--/product-details-->
+
+    <div class="category-tab shop-details-tab">
+        <!--category-tab-->
+        <div class="col-sm-12">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#details" data-toggle="tab">Description</a></li>
+                <li><a href="#companyprofile" data-toggle="tab">Details</a></li>
+
+                <li><a href="#reviews" data-toggle="tab">Rating</a></li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <div class="tab-pane fade active in" id="details">
+                <p>{!! $product->description!!}</p>
+            </div>
+
+            <div class="tab-pane fade" id="companyprofile">
+                <p>{!! $product->content !!}</p>
+            </div>
+
+            <div class="tab-pane fade" id="reviews">
+                <div class="col-sm-12">
+                    <ul>
+                        <li><a href=""><i class="fa fa-user"></i>EUGEN</a></li>
+                        <li><a href=""><i class="fa fa-clock-o"></i>12:41 PM</a></li>
+                        <li><a href=""><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
+                    </ul>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p><b>Write Your Review</b></p>
+
+                    <form action="#">
+                        <span>
+                            <input type="text" placeholder="Your Name" />
+                            <input type="email" placeholder="Email Address" />
+                        </span>
+                        <textarea name=""></textarea>
+                        <b>Rating: </b> <img src="images/product-details/rating.png" alt="" />
+                        <button type="button" class="btn btn-default pull-right">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!--/category-tab-->
+
+    <div class="recommended_items">
+        <!--recommended_items-->
+        <h2 class="title text-center">Related Products</h2>
+
+        <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="item active">
+                    @foreach ($product->category->products as $relate)
+                        @if ($relate->id == $product->id )
+                            @continue
+                        @endif
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center product-related">
+                                        <img src="{{ URL::to('public/uploads/product/' . $relate->image) }}" alt="" />
+                                        <h2>{{ number_format($relate->price, 0, ',', '.') . ' ' . 'VNĐ' }}</h2>
+                                        <p>{{ $relate->name }}</p>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.add-to-cart').click(function() {
+                var id = $(this).data('id_product');
+
+                var cart_product_id = $('.cart_product_id_' + id).val();
+                var cart_product_name = $('.cart_product_name_' + id).val();
+                var cart_product_image = $('.cart_product_image_' + id).val();
+                var cart_product_quantity = $('.cart_product_quantity_' + id).val();
+                var cart_product_price = $('.cart_product_price_' + id).val();
+                var cart_product_qty = $('.cart_product_qty_' + id).val();
+                var _token = $('input[name="_token"]').val();
+
+                if (parseInt(cart_product_qty) > parseInt(cart_product_quantity)) {
+                    alert('Làm ơn đặt nhỏ hơn ' + cart_product_quantity);
+                } else {
+                    $.ajax({
+                        url: '{{ url('/add-cart') }}',
+                        method: 'POST',
+                        data: {
+                            cart_product_id: cart_product_id,
+                            cart_product_name: cart_product_name,
+                            cart_product_image: cart_product_image,
+                            cart_product_price: cart_product_price,
+                            cart_product_qty: cart_product_qty,
+                            _token: _token,
+                            cart_product_quantity: cart_product_quantity
+                        },
+                        success: function() {
+
+                            swal({
+                                    title: "Đã thêm sản phẩm vào giỏ hàng",
+                                    text: "Bạn có thể mua hàng tiếp hoặc tới giỏ hàng để tiến hành thanh toán",
+                                    showCancelButton: true,
+                                    cancelButtonText: "Xem tiếp",
+                                    confirmButtonClass: "btn-success",
+                                    confirmButtonText: "Đi đến giỏ hàng",
+                                    closeOnConfirm: false
+                                },
+                                function() {
+                                    window.location.href = "{{ url('/show-cart') }}";
+                                });
+                        }
+
+                    });
+                }
+
+            })
+        });
+    </script>
+@endsection
