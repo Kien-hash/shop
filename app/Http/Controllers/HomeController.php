b3e77004e4c6594017a18c37e69d5315bd48b632
Kien <ntkien.hust@gmail.com>
2022-01-20 01:56:35 +0700

    send mail

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,6 @@ use Illuminate\Http\Request;
 use App\Brand;
 use App\Category;
 use App\Product;
-use Mail;
-use App\Order;
-use App\Coupon;
 
 class HomeController extends Controller
 {
@@ -61,31 +58,5 @@ class HomeController extends Controller
         $product = Product::where('slug', '=', $slug)->first();
         $relates = Product::where('category_id', $product->category_id)->where('status', 0)->where('id', '!=', $product->id)->paginate(3);
         return view('pages.home.detail', ['product' => $product, 'categories' => $categories, 'brands' => $brands, 'relates' => $relates]);
-    }
-
-    public function sendMail()
-    {
-        // $to_name = "Hieu Tan Tutorial";
-        // $to_email = "ntkien.hust@gmail.com"; //send to this email
-
-        // $data = array("name" => "Mail từ tài khoản Khách hàng", "body" => 'Mail gửi về vấn về hàng hóa'); //body of mail.blade.php
-
-        // Mail::send('pages.mail.index', $data, function ($message) use ($to_name, $to_email) {
-
-        //     $message->to($to_email)->subject('Test thử gửi mail google'); //send this mail with subject
-        //     $message->from($to_email, $to_name); //send from this mail
-        // });
-        $order = Order::find(1);
-        $coupon = Coupon::where('code', $order->coupon)->first();
-        // Send mail to customer
-        $to_name = 'Shop Bán Hàng';
-        $to_email = 'ntkien.hust@gmail.com'; //send to this email
-
-        $data = array('order' => $order, 'coupon' => $coupon); //body of mail.blade.php
-
-        Mail::send('pages.mail.index', $data, function ($message) use ($to_name, $to_email) {
-            $message->to($to_email)->subject('Thông báo đặt hàng thành công'); //send this mail with subject
-            $message->from($to_email, $to_name); //send from this mail
-        });
     }
 }
